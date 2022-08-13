bf422e9a18314fe116c8f9afda36cccb03ef7cfa
Kosta Kazakov <kostakazakoff@gmail.com>
2022-08-13 17:39:35 +0300

    Update main.py

--- file_cleaner/main.py
+++ file_cleaner/main.py
@@ -10,27 +10,27 @@ class Duplicated:
         self.list_of_files = []
 
     def find_duplicated_files(self, sys_path):
-        '''
+        """
         creating a list of all files from the entered system path:
         file_names contains just the names of files
         list_of_files contains the full path
-        '''
+        """
         for root, dirs, files in os.walk(sys_path):
             for name in files:
                 if fnmatch.fnmatch(name, '*.*'):
                     self.file_names.append(name)
                     self.list_of_files.append(os.path.join(root, name))
-        '''
+        """
         using the file_names list for searching the duplicates
         and using the index (dupl_index) to operate in the list_of_files
         because the equal indexes of the same files in both lists:
-        '''
+        """
         for name in self.file_names:
             files_counter = self.file_names.count(name)
-            '''
+            """
             using the counter - using this iteration,
             checking if they are a duplicated files by file of the names in file_names list:
-            '''
+            """
             if files_counter > 1:
                 for i in range(files_counter):
                     dupl_index = self.file_names.index(name)
